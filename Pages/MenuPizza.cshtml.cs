4c943129ac01ff927be2b3023a648c93b1330c8d
FayadIdrissB <fayad-idriss.bacari@outlook.com>
2025-01-27 15:08:07 +0100

    ajout des données

---
+++ Pages/MenuPizza.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PizzaFamily.Models;
+
+namespace PizzaFamily.Pages
+{
+	public class MenuPizzaModel : PageModel
+    {
+        private readonly PizzaFamily.Data.DataContext _context;
+
+        public MenuPizzaModel(PizzaFamily.Data.DataContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Pizza> Pizza { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Pizza = await _context.Pizzas.ToListAsync();
+        }
+    }
+}
